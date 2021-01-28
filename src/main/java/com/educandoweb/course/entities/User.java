cd74adbf93b9007e4419c3ddb569f15f5362f053
Klecia Vladmaker <klecia.vladmaker@hotmail.com>
2021-01-27 22:22:34 -0300

    Entidade Pedido. datas com Instant e Padrão ISO 8601 - Parte 2

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -11,6 +11,10 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+
+
 @Entity
 @Table(name = "tb_user")
 public class User implements Serializable {
@@ -24,6 +28,7 @@ public class User implements Serializable {
 	private String phone;
 	private String password;
 
+	@JsonIgnore
 	@OneToMany(mappedBy = "client")
 	private List<Order> orders = new ArrayList<>();
 
