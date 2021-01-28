1c2a25f7ba82c7c05c214cc5ba359d91ed126cc4
Klecia Vladmaker <klecia.vladmaker@hotmail.com>
2021-01-28 13:01:16 -0300

    Métodos subtotal e total

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -108,6 +108,14 @@ public class Order implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+	
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
 
 	@Override
 	public boolean equals(Object obj) {
